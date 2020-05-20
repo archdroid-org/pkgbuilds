95b13d9514c2abdde4c1c6624bae81c8fd7615a4
jgmdev <jgmdev@gmail.com>
2020-05-20 14:52:00 -0400

    Some changes for consistency.

---
+++ odroid-gl4es/gl4es.sh
@@ -0,0 +1,5 @@
+export LIBGL_EGL=/usr/lib/mali-egl/libmali.so
+export LIBGL_GLES=/usr/lib/mali-egl/libmali.so
+export LIBGL_FB=1
+export LIBGL_GL=21
+export LIBGL_XREFRESH=1
--- odroid-n2-gl4es/gl4es.sh
+++
@@ -1,5 +0,0 @@
-export LIBGL_EGL=/usr/lib/libMali.so
-export LIBGL_GLES=/usr/lib/libMali.so
-export LIBGL_FB=1
-export LIBGL_GL=21
-export LIBGL_XREFRESH=1
